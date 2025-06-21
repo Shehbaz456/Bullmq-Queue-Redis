0354a74984890e19b56d6ff761f82869f3906ff1
shehbaz <mdshehbaz666666@gmail.com>
2025-06-21 13:36:55 +0530

    feat(queue): enhance video processing worker with wait functionality

--- queues/worker.js
+++ queues/worker.js
@@ -1,13 +1,21 @@
-import { Worker } from 'bullmq';
-import { QueueMap } from './queue.js';
-import { redisConnection } from '../connection.js';
+import { Worker } from "bullmq";
+import { QueueMap } from "./queue.js";
+import { redisConnection } from "../connection.js";
 
-export const videoProcessingworker = new Worker(QueueMap["VIDEO_PROCEESSING_QUEUE"],
-    async (job) => {
-        console.log(`Processing Job`,job.id);
-        console.log(`Transcoding Job`,{url:job.data});
 
-        return true;
-    },
-    { autorun:false,connection:redisConnection }
+const wait = (s) => new Promise((res)=>setTimeout(res, s*1000))
+
+
+export const videoProcessingworker = new Worker(
+  QueueMap["VIDEO_PROCEESSING_QUEUE"],
+  async (job) => {
+    console.log(`Processing Job`, job.id);
+    console.log(`Transcoding Job`, { url: job.data });
+    
+    await wait(10);
+    console.log(`Transcoding Job Done....`, { url: job.data });
+
+    return true;
+  },
+  { autorun: false, connection: redisConnection }
 );
