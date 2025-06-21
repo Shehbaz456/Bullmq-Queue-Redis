b9b1eaaf015a80c75580060cdbcbd3d4053ee69f
shehbaz <mdshehbaz666666@gmail.com>
2025-06-21 14:13:11 +0530

    Queue system with BullMQ decoupling micro service architecture
    -feat(queue): add notification worker and integrate with video processing

--- queues/queue.js
+++ queues/queue.js
@@ -3,9 +3,14 @@ import { redisConnection } from "../connection.js";
 
 export const QueueMap = {
   VIDEO_PROCEESSING_QUEUE: "VIDEO_PROCESSING_QUEUE",
+  NOTIFICATION_QUEUE: "NOTIFICATION_QUEUE",
 };
 
 export const videoProcessingQueue = new Queue(
   QueueMap["VIDEO_PROCEESSING_QUEUE"],
+  { connection: redisConnection }
+);
+export const notificationQueue = new Queue(
+  QueueMap["NOTIFICATION_QUEUE"],
   { connection: redisConnection }
 );
--- queues/worker.js
+++ queues/worker.js
@@ -1,21 +1,39 @@
 import { Worker } from "bullmq";
-import { QueueMap } from "./queue.js";
+import { QueueMap, notificationQueue } from "./queue.js";
 import { redisConnection } from "../connection.js";
 
-
-const wait = (s) => new Promise((res)=>setTimeout(res, s*1000))
-
+const wait = (s) => new Promise((res) => setTimeout(res, s * 1000));
 
 export const videoProcessingworker = new Worker(
   QueueMap["VIDEO_PROCEESSING_QUEUE"],
   async (job) => {
     console.log(`Processing Job`, job.id);
     console.log(`Transcoding Job`, { url: job.data });
-    
-    await wait(10);
-    console.log(`Transcoding Job Done....`, { url: job.data });
+
+    await wait(4);
+    console.log(`Transcoding Job Done...........`);
+
+    await notificationQueue.add(`notification-${job.data.videoURL}`, {
+      notification: `video has been processed for ${job.data.videoURL}`,
+    });
 
     return true;
   },
   { autorun: false, connection: redisConnection }
+);
+
+export const notificationWorker = new Worker(
+  QueueMap["NOTIFICATION_QUEUE"],
+  async (job) => {
+    console.log(`Sending Notification: ${job.data.notification}`);
+  },
+  {
+    connection: redisConnection,
+    autorun: false,
+    concurrency: 1,
+    limiter: {
+      max: 5,
+      duration: 10 * 1000,
+    },
+  }
 );
